fce1ad57ca4aced6d07804baf2731405af4743f0
Thomas Andrei <thomasandrei1991@gmail.com>
2026-02-24 22:50:17 +0800

    Updated

--- SampleProject4.py
+++ SampleProject4.py
@@ -8,11 +8,9 @@ print("3. Quit")
 print()
 while True:
     choice = input("Enter choice : ").strip()
-
     if choice == "1":
         task = input("Enter task : ")
         tasks.append(task)
-        print("Task added!\n")
     elif choice == "2":
         print("Your tasks:")
         for i, task in enumerate(tasks, 1):
@@ -21,3 +19,5 @@ while True:
     elif choice == "3":
         print("Goodbye!")
         break
+    else:
+        print("Wrong input!")
--- SampleProject6.py
+++ SampleProject6.py
@@ -1,22 +1,34 @@
-import random
+print("Contact Book Menu:")
+print("1. Add contact")
+print("2. View all contacts")
+print("3. Search contact")
+print("4. Exit")
+
+names = []
+phones = []
+emails = []
 
 while True:
+    choice = input("\nChoice : ").strip()
+    if choice == "1":
+        name = input("Name : ")
+        phone = input("Phone : ")
+        email = input("Email : ")
+        print("Contact Added!")
+        names.append(name)
+        phones.append(phone)
+        emails.append(email)
 
-    secret = random.randint(1, 100)   # NEW number per game
-    print("New game started!")
+    elif choice == "2":
+        count = 0
+        print("Contacts:")
+        for name, phone, email in zip(names, phones, emails):
+            count += 1
+            print(f"{count}. {name:10} {phone:2} \t {email}")
 
-    while True:
-        guess = int(input("Guess: "))
-
-        if guess > secret:
-            print("Too high!")
-        elif guess < secret:
-            print("Too low!")
-        else:
-            print("Correct!")
-            break
-
-    again = input("Play again? (y/n): ")
-
-    if again.lower() != 'y':
+    elif choice == "4":
+        print("Goodbye!")
         break
+    else:
+        print("Invalid Choice!")
+
